6a190514afc6cd06fac4fbf2e5f8dc5f30a455dd
Casper The Cat <zsmyna@gmail.com>
2015-09-23 15:19:05 +1200

    More work on Perlin Added reference

--- HeightmapGenerator/Perlin.cpp
+++ HeightmapGenerator/Perlin.cpp
@@ -1,8 +1,10 @@
 #include "Core.h"
 #include <numeric>
+#include <algorithm>
 
 namespace Heightmap
 {
+	//* https://github.com/sol-prog/Perlin_Noise/blob/master/PerlinNoise.cpp *//
 
 	//////////////////////////////////////////////////
 	// Perlin Constructor
@@ -16,8 +18,41 @@ namespace Heightmap
 		std::iota(this->vecParam.begin(), this->vecParam.end(), 0);
 
 		/// Create Random generator
+		std::default_random_engine rGen(seed);
 
+		/// Shuffle vector using rGen engine
+		std::shuffle(this->vecParam.begin(), this->vecParam.end(),rGen);
 
+		/// Dup Vector
+		this->vecParam.insert(this->vecParam.end(),this->vecParam.begin(), this->vecParam.end());
+	}
+
+	//////////////////////////////////////////////////
+	// Generate Noise
+	//
+	double Perlin::generateNoise(double xCoord, double yCoord, double zCoord)
+	{
+		uint8_t ixCoord = static_cast<int>(floor(xCoord)) & 255;
+		uint8_t iyCoord = static_cast<int>(floor(yCoord)) & 255;
+		uint8_t izCoord = static_cast<int>(floor(zCoord)) & 255;
+
+		xCoord -= floor(xCoord);
+		yCoord -= floor(yCoord);
+		zCoord -= floor(zCoord);
+
+		auto x = fade(xCoord);
+		auto y = fade(yCoord);
+		auto z = fade(zCoord);
+
+		auto pA = this->vecParam[ixCoord] + iyCoord;
+		auto pAa = this->vecParam[pA] + izCoord;
+		auto pAb = this->vecParam[pA + 1] + izCoord;
+
+		auto pB = this->vecParam[ixCoord + 1] + iyCoord;
+		auto pBa = this->vecParam[pB] + izCoord;
+		auto pBb = this->vecParam[pB + 1] + izCoord;
+
+		return 0;
 	}
 
 
